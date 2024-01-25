36c9b24519e8c06af066e817b3a1b20662f3bb7f
Jordan Duran <jordanduranm@gmail.com>
2024-01-25 11:21:37 -0500

    Fetching particular records

--- src/app/people/[id]/page.tsx
+++ src/app/people/[id]/page.tsx
@@ -1,6 +1,26 @@
-const DisplayPersonPage = (props: any) => {
-  console.log(props);
-  return <div>Show person</div>;
+import { notFound } from 'next/navigation';
+import { db } from '@/db';
+
+interface DisplayPersonPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const DisplayPersonPage = async (props: DisplayPersonPageProps) => {
+  const person = await db.person.findFirst({
+    where: { id: parseInt(props.params.id) },
+  });
+
+  if (!person) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      {person.firstName} {person.lastName}
+    </div>
+  );
 };
 
 export default DisplayPersonPage;
